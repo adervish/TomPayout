9b41ff654a654f70c6fcb4b7aa5deb7cd14f3bc4
Alex Derbes <xor@bentboolean.com>
2023-06-22 11:44:40 -0400

    fixed number formatting

--- payout.py
+++ payout.py
@@ -30,13 +30,17 @@
 
 import math
 import streamlit as st
+import locale 
+
+locale.setlocale(locale.LC_ALL, '')
+
 
 st.title("Winning Payout Calculator 2023")
 
 with st.form("my_form"):
-    p1 = int(st.number_input('Player one chips'))
-    p2 = int(st.number_input('Player two chips'))
-    pot = int(st.number_input('Total chips'))
+    p1 = int(st.number_input('Player one chips:'))
+    p2 = int(st.number_input('Player two chips:'))
+    pot = int(st.number_input('Total dollars:'))
     p = [p1, p2]
     submitted = st.form_submit_button("Submit")
     
@@ -46,6 +50,8 @@ with st.form("my_form"):
         low_payout = math.ceil(r * pot/5.) * 5
         payouts = [low_payout, pot - low_payout]
         if p2 == min(p): payouts = list(reversed(payouts))
-        st.write("P1: %d" % (payouts[0]))
-        st.write("P2: %d" % (payouts[1]))
+        p1_dollars = locale.currency(payouts[0], grouping=True)
+        p2_dollars = locale.currency(payouts[1], grouping=True)
+        st.write(f"P1: {p1_dollars}")
+        st.write(f"P2: {p2_dollars}")
     
